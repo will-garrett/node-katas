02b81f6f965f5a1acad47c9e64e9dd8464814ecf
Will Garrett <will.garrett2k@gmail.com>
2018-08-27 20:27:53 -0700

    Level Width

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let counters = [0];
+    let ray = [root,'stop'];
+    
+    while(ray.length > 1){
+        let node = ray.shift();
+        if(node == 'stop'){
+            counters.push(0);
+            ray.push('stop');
+        }
+        else{
+            ray.push(...node.children);
+            counters[counters.length-1]++;
+        }
+    } 
+    return counters;
+}
 
 module.exports = levelWidth;
