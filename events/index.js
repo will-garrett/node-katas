a2d7743cbfee629b52e758f651d81c6dd42e79c1
Will Garrett <will.garrett2k@gmail.com>
2018-08-27 21:31:31 -0700

    Event Library

--- events/index.js
+++ events/index.js
@@ -4,16 +4,31 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor(){
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(!this.events[eventName]){
+      this.events[eventName]=[];
+    }
+    this.events[eventName].push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    for(let x of this.events[eventName]){
+      x();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events[eventName] = [];
+//    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
