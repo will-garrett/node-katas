82043aa0c0737b2f08ee911e38d486e1c45f4006
Will Garrett <will.garrett2k@gmail.com>
2018-08-27 18:26:58 -0700

    Circular linked list test

--- circular/index.js
+++ circular/index.js
@@ -12,6 +12,17 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    let slow = list.head;
+    let fast = list.head;
+    while(fast.next != null && fast.next.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            return true;
+        }
+    }
+    return false;
+}
 
 module.exports = circular;
