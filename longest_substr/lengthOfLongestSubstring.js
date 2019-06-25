31326ccdf17141ddd16b787853f12b477e513aa7
Will Garrett <will.garrett2k@gmail.com>
2019-06-25 00:55:23 -0700

    Longest Substr problem

--- longest_substr/lengthOfLongestSubstring.js
+++ longest_substr/lengthOfLongestSubstring.js
@@ -1,39 +1,15 @@
 var lengthOfLongestSubstring = function(s) {
-    let l=0;
-    let r = 1;
+    let longest = 0;
+    let l = 0;
+    let r = 0;
     let hash = {};
-    if(s.length == 0){
-      return 0;
-    }
-    let temp = s[0];
-    let longest = s[0];
-    for(l = 0; l < s.length; l++){
-      r = l+1;
-      hash[s[l]]=1;
-      while(r < s.length){
-        if(!hash.hasOwnProperty(s[r])){
-          hash[s[r]] = 1;
-          temp = s.slice(l, r+1);
-        }
-        else{
-          if(temp.length > longest.length){
-            longest = temp;
-          
-          }
-          hash = {};
-          temp = '';
-          break;
-        }
-        if(r == s.length - 1){
-          if(temp.length > longest.length){
-            longest = temp;
-          }
-          hash = {};
-          temp = '';
-        }
-        r++;
+    for(r = 0; r < s.length; r++){
+      if(hash.hasOwnProperty(s[r])) {
+        l = Math.max(hash[s[r]], l);
       }
+      longest = Math.max(longest, r - l + 1);
+      hash[s[r]] = r + 1;
     }
-    return longest.length;
+    return longest;
 };
 module.exports = lengthOfLongestSubstring;
