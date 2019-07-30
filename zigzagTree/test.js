fa02080c22777e0c8356ce33a247806c0cbf6b45
Will Garrett <will.garrett2k@gmail.com>
2019-07-30 01:26:14 -0700

    Zig Zag Two Ways

--- zigzagTree/test.js
+++ zigzagTree/test.js
@@ -3,12 +3,16 @@ function TreeNode(val) {
   this.left = this.right = null;
 }
 const zigzag = require("./zigzag");
-
-test('should return a zig zag', () => {
+describe('Zig Zag Recursive', () => {
   let root = new TreeNode(3);
   root.left = new TreeNode(9);
   root.right = new TreeNode(20);
   root.right.right= new TreeNode(7);
   root.right.left= new TreeNode(15);
-  expect(zigzag(root)).toEqual([[3],[20,9],[15,7]]);
+  test('should return a zig zag recursively', () => {
+    expect(zigzag.recursive(root)).toEqual([[3],[20,9],[15,7]]);
+  });
+  test('should return a zig zag iteratively', () => {
+    expect(zigzag.iterative(root)).toEqual([[3],[20,9],[15,7]]);    
+  });
 });
--- zigzagTree/zigzag.js
+++ zigzagTree/zigzag.js
@@ -18,7 +18,9 @@ return its zigzag level order traversal as:
   [15,7]
 ]
 */
-module.exports = function(root){
+
+/* Solutions */
+module.exports.iterative = function(root){
   let results = [];
   let currentLvl = [];
   let nextLvl = [];
@@ -59,3 +61,27 @@ module.exports = function(root){
   }
   return results;
 }
+
+module.exports.recursive = function(root) {
+  var helper = function(result, level, curr) {
+      if (curr === null) {
+          return;
+      }
+      
+      if (!result[level]) result[level] = [];
+      
+      let leftToRight = level % 2 === 0;
+      if (leftToRight) {
+          result[level].push(curr.val);
+      } else {
+          result[level].unshift(curr.val);
+      }
+      
+      helper(result, level+1, curr.left);
+      helper(result, level+1, curr.right);
+  }
+  
+  let result = [];
+  helper(result, 0, root);
+  return result;
+};
