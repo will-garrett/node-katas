21059fc1ddac03c8778bb8f685a062458c5f7100
Will Garrett <will.garrett2k@gmail.com>
2019-09-18 00:17:24 -0700

    zig zag tree additional test

--- zigzagTree/test.js
+++ zigzagTree/test.js
@@ -3,7 +3,7 @@ function TreeNode(val) {
   this.left = this.right = null;
 }
 const zigzag = require("./zigzag");
-describe('Zig Zag Recursive', () => {
+describe('Zig Zag BST', () => {
   let root = new TreeNode(3);
   root.left = new TreeNode(9);
   root.right = new TreeNode(20);
@@ -14,5 +14,17 @@ describe('Zig Zag Recursive', () => {
   });
   test('should return a zig zag iteratively', () => {
     expect(zigzag.iterative(root)).toEqual([[3],[20,9],[15,7]]);    
+  });
+});
+describe('Zig zag BST', ()=>{
+  let root = new TreeNode(3);
+  root.left = new TreeNode(9);
+  root.right = new TreeNode(20);
+  root.left.left = new TreeNode(7);
+  root.left.right = new TreeNode(15);
+  root.right.left = new TreeNode(8);
+  root.right.right = new TreeNode(9);
+  test('output zig zag order', () => {
+    expect(zigzag.recursive(root)).toEqual([[3], [20, 9], [7, 15, 8, 9]]);
   });
 });
