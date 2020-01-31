50f422715fa8b3c97f94d5b36a4bf96c070d4e9b
Will Garrett <will.garrett2k@gmail.com>
2020-01-30 23:06:00 -0800

    Flood fill

--- floodFill/test.js
+++ floodFill/test.js
@@ -3,7 +3,8 @@
 const floodFill = require("./floodFill");
 
 test('should flood fill a 3x3', () => {
-  expect(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2)).toEqual([[2,2,2],[2,2,0],[2,0,1]]);
+  expect(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2))
+  .toEqual([[2,2,2],[2,2,0],[2,0,1]]);
 });
 
 test('should flood fill a 3x2', () => {
@@ -11,5 +12,6 @@ test('should flood fill a 3x2', () => {
     [ 0, 0, 0 ], 
     [ 0, 1, 1 ]
   ];
-  expect(floodFill([[0,0,0],[0,1,1]], 1, 1, 1)).toEqual([[0,0,0],[0,1,1]]);
+  expect(floodFill([[0,0,0],[0,1,1]], 1, 1, 1))
+  .toEqual([[0,0,0],[0,1,1]]);
 });
